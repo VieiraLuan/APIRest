f5de5c9ec07221a4d9a854877d561a4953c63628
VieiraLuan <105254554+VieiraLuan@users.noreply.github.com>
2022-07-28 18:23:58 -0300

    cleaning code

--- APIRest/Controllers/UsuarioController.cs
+++ APIRest/Controllers/UsuarioController.cs
@@ -53,36 +53,6 @@ namespace APIRest.Controllers
         }
 
 
-
-
-        /*-------------------------------------------------------------------------*/
-        /*
-
-        // GET api/<UsuarioController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<UsuarioController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<UsuarioController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<UsuarioController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-
-        */
+        
     }
 }
